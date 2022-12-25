469ab2aae48380bdf43e1745e81fa0c71922a334
sagar-kpr <sagarkpr6@gmail.com>
2022-12-25 16:30:12 +0530

    added db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
-mongoose.connect(`mongodb+srv://sagarkpr:sagar123@cluster0.odss0vq.mongodb.net/?retryWrites=true&w=majority`);
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/mynews");
 
 const db = mongoose.connection;
 
